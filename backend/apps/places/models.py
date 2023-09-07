d1d0fad5bad7c10d87db4679fa509984610b2612
charlie@camba.coop <charlie@camba.coop>
2023-09-07 20:17:32 -0300

    create Place model and migration

--- backend/apps/places/models.py
+++ backend/apps/places/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Place(models.Model):
     name = models.CharField(
-        gettext_lazy("place_prop_name"), max_length=30, unique=True
+        max_length=30, unique=True
     )
     email_contact = models.EmailField(
-        gettext_lazy("place_email_address"), max_length=80, required=False
+         max_length=80, unique=True
     )
